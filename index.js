fb296e50a2ab8e13816eaf8a51e1294dd28f74a7
Alexander Wachowiak <wachowiak@Alexanders-MacBook-Pro.local>
2023-03-14 14:02:37 +0100

    0.0.2

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const { response } = require('express');
 const express = require('express');
+const { IncomingMessage } = require('http');
 const { readFile } = require('fs').promises;
 
 const app = express();
+app.use(express.json())
 
 app.get('/', async (request, response) => {
     response.send(await readFile('./home.html', 'utf8'));
 });
 
 app.get('/input', async (request, response) => {
-    app.send("https://google.com");
+    const { message } = request.body;
+    if (!message) {
+        response.status(418).send();
+    }
+    response.send({
+        "answer": 'chatgptoutput'
+    })
 });
 
 app.listen(process.env.PORT || 3000, () => console.log('App available at http://localhost:3000'))
