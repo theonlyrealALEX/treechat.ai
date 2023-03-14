e4729fe89c35f03fa0365997804b9eea38d2bf0b
Alexander Wachowiak <wachowiak@Alexanders-MacBook-Pro.local>
2023-03-14 13:46:35 +0100

    0.0.1

--- index.js
+++ index.js
@@ -8,4 +8,8 @@ app.get('/', async (request, response) => {
     response.send(await readFile('./home.html', 'utf8'));
 });
 
+app.get('/input', async (request, response) => {
+    app.send("https://google.com");
+});
+
 app.listen(process.env.PORT || 3000, () => console.log('App available at http://localhost:3000'))
--- my-modules.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    science: 'lol'
-}
