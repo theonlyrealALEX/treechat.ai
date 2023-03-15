2a7a33b34883c6a09467f54a6870bd5f742ff37b
Alexander Wachowiak <wachowiak@Alexanders-MacBook-Pro.local>
2023-03-15 08:16:01 +0100

    0.0.4, web communication and api calls are working

--- index.js
+++ index.js
@@ -2,8 +2,39 @@ const { response } = require('express');
 const express = require('express');
 const { IncomingMessage } = require('http');
 const { readFile } = require('fs').promises;
-
+const { Configuration, OpenAIApi } = require("openai");
+const { apiKey } = require('./api_key.js');
 const app = express();
+
+const configuration = new Configuration({
+    apiKey: apiKey,
+});
+
+//TEST STARt
+async function testCompletion() {
+    const openai = new OpenAIApi(configuration);
+
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ 'role': "user", "content": "How are you?" }],
+    });
+    console.log(completion.data.choices[0].message);
+}
+// Call the async function
+//testCompletion();
+//TEST END
+
+async function getCompletion(inputMessage) {
+    const openai = new OpenAIApi(configuration);
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ 'role': "user", "content": inputMessage }],
+    });
+    console.log(completion.data.choices[0].message)
+    console.log("API Call sucessfull")
+    return completion.data.choices[0].message;
+}
+
 app.use(express.json())
 
 app.get('/', async (request, response) => {
@@ -11,16 +42,14 @@ app.get('/', async (request, response) => {
 });
 
 app.post('/input', async (request, response) => {
-    const { message } = request.body;
-    console.log(message)
-    //do chatgpt magic here with message :)
-    if (!message) {
+    console.log(request.body['value'])
+    if (!request.body) {
         response.status(418).send();
         return;
     }
-    response.send({
-        "answer": 'chatgptoutput'
-    })
+    const completionMessage = await getCompletion(request.body['value']);
+    response.send(completionMessage);
+    console.log("API response sucessfully sent out")
 });
 
 app.listen(process.env.PORT || 3000, () => console.log('App available at http://localhost:3000'))
